e8630ef90e754cf7b328f1a58e68b974ff380f8d
ap4y <lod@pisem.net>
2014-07-24 21:51:05 +1200

    Rename FromJson to JSONDecodable, use new stack approach

---
+++ JsonSerialize/JSONDecodable.swift
@@ -0,0 +1,58 @@
+public protocol JSONDecodable {
+    class func decode(decoder: JSONDecoder) -> Self?
+}
+
+extension Int: JSONDecodable {
+    public static func decode(decoder: JSONDecoder) -> Int? {
+        switch decoder.pop() {
+        case let .Number(number):
+            return Int(number)
+        default:
+            return nil
+        }
+    }
+}
+
+extension Double: JSONDecodable {
+    public static func decode(decoder: JSONDecoder) -> Double? {
+        switch decoder.pop() {
+        case let .Number(number):
+            return number
+        default:
+            return nil
+        }
+    }
+}
+
+extension String: JSONDecodable {
+    public static func decode(decoder: JSONDecoder) -> String? {
+        switch decoder.pop() {
+        case let .String(string):
+            return string
+        default:
+            return nil
+        }
+    }
+}
+
+extension Bool: JSONDecodable {
+    public static func decode(decoder: JSONDecoder) -> Bool? {
+        switch decoder.pop() {
+        case let .Number(number):
+            return Bool(number)
+        default:
+            return nil
+        }
+    }
+}
+
+extension NSDate: JSONDecodable {
+    public class func decode(decoder: JSONDecoder) -> NSDate? {
+        switch decoder.pop() {
+        case let .Number(interval):
+            return NSDate(timeIntervalSince1970: interval)
+        default:
+            return nil
+        }
+    }
+}
